bbc0736072313237b5ccbf68908d32842dd86e82
Ian F <ian.fiddeman@gmail.com>
2021-10-12 08:39:31 +0100

    odd or even chal added

--- ternary/src/com/intellij/Main.java
+++ ternary/src/com/intellij/Main.java
@@ -1,11 +1,16 @@
 package com.intellij;
 
 import static com.intellij.TrafficLight.updateLight;
+import static com.intellij.OddOrEven.simpleMultiplication;
 
 public class Main {
 
     public static void main(String[] args) {
-        var lightChange= updateLight("red");
-        System.out.println(lightChange == "green");
+//        var lightChange= updateLight("red");
+//        System.out.println(lightChange == "green");
+
+        var oddOrEven = simpleMultiplication(3);
+        System.out.println(oddOrEven == 27);
+
     }
 }
---
+++ ternary/src/com/intellij/OddOrEven.java
@@ -0,0 +1,11 @@
+package com.intellij;
+
+//Description:
+//This kata is about multiplying a given number
+// by eight if it is an even number and by nine otherwise.
+
+public class OddOrEven {
+    public static int simpleMultiplication(int n) {
+        return n % 2 == 0 ? n*8 : n*9;
+    }
+}
