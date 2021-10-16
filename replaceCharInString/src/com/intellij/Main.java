1715637594d9a9ecd917931a514113a576c748d2
Ian F <ian.fiddeman@gmail.com>
2021-10-16 19:39:44 +0100

    description added

--- replaceCharInString/src/com/intellij/Main.java
+++ replaceCharInString/src/com/intellij/Main.java
@@ -2,6 +2,8 @@ package com.intellij;
 
 public class Main {
 
+    //Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
     public static void main(String[] args) {
 	var res1 = ReplaceCharInString.removeExclamationMarks("Get rid of these '!' will you ?!");
         System.out.println(res1);
